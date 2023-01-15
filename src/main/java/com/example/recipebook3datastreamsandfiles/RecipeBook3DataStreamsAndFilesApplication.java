8dc749935ebfc023a943f4d8b185470698548a9a
Aleksandr Deev <geshtait@mail.ru>
2023-01-15 18:23:09 +0500

    Первый коммит

---
+++ src/main/java/com/example/recipebook3datastreamsandfiles/RecipeBook3DataStreamsAndFilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipebook3datastreamsandfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBook3DataStreamsAndFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBook3DataStreamsAndFilesApplication.class, args);
+	}
+
+}
